225be9a73082b6daa191f0d408141b17f3db51e9
CreativeGP <malicktafaminecraft@gmail.com>
2018-03-04 18:42:08 +0900

    Don't execute program until there is no error

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,9 @@
 pushd ./ > /dev/null
 cd ../build
 
-g++ -g -o imana ../src/main.cpp ../src/numerical_formula/fraction.cpp
-
-echo ""
-echo ""
-./imana
+g++ -g -o imana ../src/main.cpp ../src/numerical_formula/fraction.cpp \
+&& echo "" \
+&& echo "" \
+&& ./imana
 
 popd > /dev/null
